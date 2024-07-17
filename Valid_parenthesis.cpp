d8432032e1d8a9fa05ade0afe37399c078fb9d6e
Aditya Raj <141154775+adiraj0902@users.noreply.github.com>
2024-07-17 22:21:39 +0530

    Create Valid_parenthesis.cpp

---
+++ Valid_parenthesis.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isValid(std::string s) {
+        std::stack<char> stack;
+        std::unordered_map<char, char> bracketMap = {{')', '('}, {']', '['}, {'}', '{'}};
+
+        for (char c : s) {
+            if (bracketMap.find(c) != bracketMap.end()) {
+                if (stack.empty() || stack.top() != bracketMap[c]) {
+                    return false;
+                }
+                stack.pop();
+            } else {
+                stack.push(c);
+            }
+        }
+
+        return stack.empty();
+    }
+};
